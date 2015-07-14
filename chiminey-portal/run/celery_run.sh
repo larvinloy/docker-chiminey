030a457f9a2c4cb889df9bc4ab57b8fcbb7701fe
Ian Thomas <ianedwardthomas@gmail.com>
2015-07-14 17:28:38 +1000

    clean up correct celery pids on restart and increase log rotate size

--- chiminey-portal/run/celery_run.sh
+++ chiminey-portal/run/celery_run.sh
@@ -22,13 +22,20 @@ main() {
     soft_time_limit=${CELERY_SOFT_TIME_LIMIT:155200}
     time_limit=${CELERY_TIME_LIMIT:115400}
     log_level=${CELERY_LOG_LEVEL:WARN}
+    host_name=`hostname`
 
 
-    trap "shutdownit" HUP INT QUIT KILL TERM
+
+    echo cleaning up pids
+    rm -vf /var/run/celery/w1_w1\@$host_name.pid
+    rm -vf /var/run/celery/w2_w2\@$host_name.pid
+    rm -vf /var/run/celery/w3_w3\@$host_name.pid
+    rm -vf /var/run/celery/w4_w4\@$host_name.pid
+
+    sleep 10
 
     echo starting celeryd
-    rm -f /var/run/celery/*.pid
-    sleep 10
+
     #su -m chiminey -c "python chiminey.py celery worker --logfile=/logs/celery.log --loglevel=DEBUG"
     #su chiminey -c "python chiminey.py celeryd_multin w1 w2 w3 w4 -l $log_level --noft-time-limit=155200 --time-limit=115400 -E -Q:w1 hightasks -Q:w2,w3,w4 default --pidfile=/var/run/celery/%n.pid --logfile=/logs/celery/%n.log  >> /logs/celery/celery.log 2>&1"
     su chiminey -c "python chiminey.py celeryd_multi restart w1 w2 w3 w4 -l $log_level --verbose --soft-time-limit=155200 --time-limit=115400 -E -Q:w1 hightasks -Q:w2,w3,w4 default --pidfile=/var/run/celery/%n_%h.pid --logfile=/logs/celery/%n_%h.log  >> /logs/celery/celery.log 2>&1"
@@ -39,10 +46,5 @@ main() {
     echo "exited $0"
 }
 
-shutdownit() {
-    echo "shutting down"
-    #su chiminey -c "python chiminey.py celeryd_multi stop w1 w2 w3 w4 -l $log_level --soft-time-limit=155200 --time-limit=115400 -E -Q:w1 hightasks -Q:w2,w3,w4 default --pidfile=/var/run/celery/%h.pid --logfile=/logs/celery/%h.log  >> /logs/celery/celery.log 2>&1"
-    exit 0
-}
 
 main "$@"
