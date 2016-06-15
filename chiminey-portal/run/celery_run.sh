f3547c686c8874c6c42e0112d53b6c94e0e69561
Ian Thomas <ianedwardthomas@gmail.com>
2016-06-15 17:05:04 +1000

    added kibana logging

--- chiminey-portal/run/celery_run.sh
+++ chiminey-portal/run/celery_run.sh
@@ -22,6 +22,7 @@ main() {
     soft_time_limit=${CELERY_SOFT_TIME_LIMIT:155200}
     time_limit=${CELERY_TIME_LIMIT:115400}
     log_level=${CELERY_LOG_LEVEL:WARN}
+    queue=${CELERY_QUEUE}
     host_name=`hostname`
 
     function clean_up {
@@ -48,7 +49,9 @@ main() {
     #su -m chiminey -c "python chiminey.py celery worker --logfile=/logs/celery.log --loglevel=DEBUG"
     #su chiminey -c "python chiminey.py celeryd_multin w1 w2 w3 w4 -l $log_level --noft-time-limit=155200 --time-limit=115400 -E -Q:w1 hightasks -Q:w2,w3,w4 default --pidfile=/var/run/celery/%n.pid --logfile=/logs/celery/%n.log  >> /logs/celery/celery.log 2>&1"
     #su chiminey -c "python chiminey.py celeryd_multi restart w1 w2 w3 w4 -l $log_level --verbose --soft-time-limit=155200 --time-limit=115400 -E -Q:w1 hightasks -Q:w2,w3,w4 default --pidfile=/var/run/celery/%n_%h.pid --logfile=/logs/celery/celery.log  >> /logs/celery/celery.log 2>&1"
-    su chiminey -c "python chiminey.py celeryd_multi restart w1 w2 w3 w4 -l $log_level --verbose --soft-time-limit=155200 --time-limit=115400 -E -Q:w1 hightasks -Q:w2,w3,w4 default --pidfile=/var/run/celery/%n_%h.pid --logfile=/logs/celery/%n_%h.log >> /logs/celery/celery.log 2>&1"
+    #su chiminey -c "python chiminey.py celeryd_multi restart w1 w2 w3 w4 -l $log_level --verbose --soft-time -limit=155200 --time-limit=115400 -E -Q:w1 hightasks -Q:w2,w3,w4 default --pidfile=/var/run/celery/%n_%h.pid --logfile=/logs/celery/%n_%h.log >> /logs/celery/celery.log 2>&1"
+    su chiminey -c "python chiminey.py celery worker -n w1.%h -l $log_level -Q $queue --pidfile=/var/run/celery/%n_%h.pid 2>&1"
+
 
     echo "waiting..."
     tail -f /dev/null
